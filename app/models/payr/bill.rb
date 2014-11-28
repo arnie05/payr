e23780166ca8815cf9b077da032ba4558249e0e4
Alban Merino <amerino@dotgee.fr>
2014-11-28 10:30:57 +0100

    Update rails 4
    remove attr_accessible for attributes, no longer used for rails 4

--- app/models/payr/bill.rb
+++ app/models/payr/bill.rb
@@ -5,6 +5,4 @@ class Payr::Bill < ActiveRecord::Base
   CANCELLED = "cancelled"
   SIGN_ERROR = "bad_signature"
   NO_ERROR = "00000"
-  
-  attr_accessible :amount, :article_id, :buyer_id, :state, :bill_reference
 end
